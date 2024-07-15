c0ad4b5cac70d7a5bc9970ed2dc114e3f1c50f84
Vasili.Mordus <v.mordus@andersenlab.com>
2024-07-15 16:22:26 +0300

    Initial Commit

---
+++ DivideSmartUITests/DivideSmartUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DivideSmartUITestsLaunchTests.swift
+//  DivideSmartUITests
+//
+//  Created by Shirayo on 15/07/2024.
+//
+
+import XCTest
+
+final class DivideSmartUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
